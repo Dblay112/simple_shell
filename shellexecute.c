3aceaeccb1c197fd4d614f2f73621b16443c7399
dblay112 <brightdjogatse@gmail.com>
2023-07-25 05:53:41 -0700

    updated codes

--- shellexecute.c
+++ shellexecute.c
@@ -1,98 +1,119 @@
 #include "shellheader.h"
 
 /**
- * stringsplit - function that splits strings
+ * shellexec - function to execute commands from info struct
+ * @info: struct parameter argument
+ * Return: Nothing
+ */
+
+void shellexec(info_t *info)
+{
+	char *p = NULL;
+	int r = 0, place = 0;
+
+	while (info->arg[r])
+	{
+		place += !shell_lim(info->arg[r], " \t\n") ? 1 : 0;
+		r++;
+	}
+
+	p = (!place) ? NULL : shellpath(info, getshell_env("PATH"), info->argv[0]);
+
+	if (p)
+	{
+		info->path = p;
+		executeshell_f(info);
+	}
+	else
+	{
+	if ((getshell_env("PATH") || info->argv[0][0] == '/')
+			&& commandsearch(info, info->argv[0]))
+		executeshell_f(info);
+	else if (*(info->arg) != '\n')
+		e_construct(info, "not found\n");
+	}
+}
+
+/**
+ * shellpath - finds path of the executed command
  * @info: a pointer to the struct info_t
- * Return: 1 on success and 0 on failure
+ * @placestr: the directory strings are input
+ * @cmd: executed command path
+ * Return: the executed command
  */
 
-int stringsplit(info_t *info)
+char *shellpath(info_t *info, char *placestr, char *cmd)
 {
-	int i;
-	(*info).argv = mimic_strtokd((*info).arg, " \t");
-		if (!(*info).argv)
+	int r = 0, posi = 0;
+	char *place;
+
+	if (!placestr)
+		return (NULL);
+	else if ((stringlength(cmd) > 2) && findcharacter(cmd, "./"))
+	{
+		if (commandsearch(info, cmd))
+			return (cmd);
+		else
+			return (NULL);
+	}
+	else
+	{
+	while (1)
+	{
+		if (!placestr[r] || placestr[r] == ':')
 		{
-			(*info).argv = malloc(sizeof(char *) * 2);
-			if ((*info).argv)
-			{
-				(*info).argv[0] = stringduplicate((*info).arg);
-				(*info).argv[1] = NULL;
-
-			}
+			place = mul_strings(placestr, posi, r);
+		if (!*place)
+			appendstring(place, cmd);
+		else
+		{
+			appendstring(place, "/");
+			appendstring(place, cmd);
 		}
-		for (i = 0; (*info).argv && (*info).argv[i]; i++)
-			;
-		(*info).argc = i;
-		return (i);
+		if (commandsearch(info, place))
+			return (place);
+		if (!placestr[r])
+			break;
+		posi = r;
+		}
+	r++;
+	}
+	return (NULL);
+	}
 }
 
 /**
- * mimic_strtokd - mimics the behaviour of strtok c libraty
- * @inputString: the strings to be inputted
- * @delimiters: delimits the strings in the function
- * Return: pointer to the result string
+ * commandsearch - finds out if the input is a command or not
+ * @info: a pointer to the struct info_t
+ * @place: location of where the command is read
+ * Return: 1 if successful or 0 on failure
  */
 
-char **mimic_strtokd(char *inputString, char *delimiters)
+int commandsearch(info_t *info, char *place)
 {
-	int index1, index2, index3, index4, numWords = 0;
-	char **result;
-	int i;
+	struct stat st;
 
-	if (inputString == NULL || inputString[0] == 0)
-		return (NULL);
-	if (!delimiters)
-		delimiters = " ";
-	for (index1 = 0; inputString[index1] != '\0'; index1++)
-		if (!shell_lim(inputString[index1], delimiters)
-				&& (shell_lim(inputString[index1 + 1], delimiters)
-					|| !inputString[index1 + 1]))
-		numWords++;
-	if (numWords == 0)
-		return (NULL);
-	result = malloc((1 + numWords) * sizeof(char *));
-	if (!result)
-		return (NULL);
-	for (index2 = 0, index3 = 0; index3 < numWords; index3++)
-	{
-		while (shell_lim(inputString[index2], delimiters))
-		index2++;
-		index4 = 0;
-		while (!shell_lim(inputString[index2 + index4], delimiters)
-				&& inputString[index2 + index4])
-			index4++;
-		result[index3] = malloc((index4 + 1) * sizeof(char));
-	if (!result[index3])
-	{
-		for (index4 = 0; index4 < index3; index4++)
-			free(result[index4]);
-		free(result);
-		return (NULL);
-	}
-	for (i = 0; i < index4; i++)
-		result[index3][i] = inputString[index2++];
-	result[index3][index4] = 0;
-	}
-	result[index3] = NULL;
-	return (result);
+	(void)info;
+	return ((!place || stat(place, &st)) ? 0 :
+		(st.st_mode & S_IFREG) ? 1 :
+		0);
 }
 
 /**
- * shell_lim - determines whether char is a delimiter or not
- * @c: character to check
- * @delim: delimiting string
- * Return: 1 when successful and 0 on failure
+ * mul_strings - function that multiplies strings
+ * @placestr: the directory strings are input
+ * @start: string commencement
+ * @stop: halting the string
+ * Return: final string worked on
  */
 
-int shell_lim(char c, char *delim)
+char *mul_strings(char *placestr, int start, int stop)
 {
-	int i = 0;
+	static char place[1024];
+	int m = 0, u = 0;
 
-	while (delim[i] != '\0')
-	{
-	if (delim[i] == c)
-		return (1);
-	i++;
-	}
-	return (0);
+	for (u = 0, m = start; m < stop; m++)
+		(placestr[m] != ':') ? (place[u++] = placestr[m]) : 0;
+	place[u] = 0;
+	return (place);
 }
